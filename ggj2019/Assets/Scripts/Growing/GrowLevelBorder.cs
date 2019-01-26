648110643cf13f69d5fe362896f82dde30a169c5
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2019-01-26 16:46:41 +0100

    Added first version of the growlevel border.

--- ggj2019/Assets/Scripts/Growing/GrowLevelBorder.cs
+++ ggj2019/Assets/Scripts/Growing/GrowLevelBorder.cs
@@ -1,41 +1,104 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GrowLevelBorder : MonoBehaviour
 {
-    [SerializeField]
-    private PlayerController m_Player;
+    [Serializable]
+    public class BreakSetting
+    {
+        [SerializeField]
+        private float m_RequiredPower;
+        public float RequiredPower
+        {
+            get { return m_RequiredPower; }
+        }
 
+        [SerializeField]
+        private Color m_VisualColor; //Will change into a sprite
+        public Color VisualColor
+        {
+            get { return m_VisualColor; }
+        }
+    }
+
+    [Header("Settings")]
+    [Space(5)]
     [SerializeField]
     private float m_Radius;
 
     [SerializeField]
+    private List<BreakSetting> m_BreakSettings;
+    private int m_CurrentBreakSetting = 0;
+
+    [Header("Required References")]
+    [Space(5)]
+    [SerializeField]
+    private PlayerController m_Player;
+
+    [SerializeField]
     private SpriteRenderer m_Visuals;
+
 
     private void Start()
     {
+        if (LevelDirector.Instance != null)
+            LevelDirector.Instance.LevelStartEvent += OnLevelStart;
+
         if (m_Player != null)
             m_Player.MoveEvent += OnPlayerMove;
     }
 
     private void OnDestroy()
     {
+        if (LevelDirector.Instance != null)
+            LevelDirector.Instance.LevelStartEvent -= OnLevelStart;
+
         if (m_Player != null)
             m_Player.MoveEvent -= OnPlayerMove;
     }
 
+    private void OnLevelStart()
+    {
+        if (m_BreakSettings.Count > 0)
+            m_Visuals.color = m_BreakSettings[0].VisualColor;
+
+        m_CurrentBreakSetting = 0;
+    }
+
     private void OnPlayerMove(PlayerController player, Vector3 newPosition)
     {
+        if (IsBroken())
+            return;
+
         Vector3 diff = newPosition - transform.position;
 
         //Check if a player is trying to leave the level
         if (diff.magnitude >= m_Radius)
         {
-            //The player doesn't have the right amount of power yet, bounce him back.
-            Vector3 direction = diff.normalized;
-            player.SetPosition(direction * m_Radius);
+            //Check if the power of the player is enough to break trough the next stage (and if there is a next stage)
+            if (m_CurrentBreakSetting >= 0 && (m_CurrentBreakSetting + 1) < m_BreakSettings.Count)
+            {
+                if (player.GetPower() >= m_BreakSettings[m_CurrentBreakSetting].RequiredPower)
+                {
+                    m_CurrentBreakSetting += 1;
+                    m_Visuals.color = m_BreakSettings[m_CurrentBreakSetting].VisualColor;
+                }
+            }
+
+            //Bounce the player back unless the border just got broken.
+            if (IsBroken() == false)
+            {
+                Vector3 direction = diff.normalized;
+                player.SetPosition(direction * m_Radius);
+            }
         }
+    }
+
+    public bool IsBroken()
+    {
+        return (m_CurrentBreakSetting >= (m_BreakSettings.Count - 1));
     }
 
     private void OnDrawGizmosSelected()
