c06724ac79302943a7774c337740fb3ad6bd227d
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2019-01-27 11:36:42 +0100

    Added fades to the growing scene.

--- ggj2019/Assets/Scripts/UI/FadeUI.cs
+++ ggj2019/Assets/Scripts/UI/FadeUI.cs
@@ -10,6 +10,8 @@ public class FadeUI : MonoBehaviour
     private void Start()
     {
         Image image = GetComponent<Image>();
+
+        image.color = new Color(image.color.r, image.color.g, image.color.b, 1.0f);
         image.DOFade(0.0f, 1.0f);
     }
 }
