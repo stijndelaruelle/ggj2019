67ba1d7bac6f75c0aad1dc013d0d84c5f4aa718a
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2019-01-27 04:25:23 +0100

    Added reset "button" for mobile.

---
+++ ggj2019/Assets/Scripts/ResetButtonTouch.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButtonTouch : MonoBehaviour
+{
+    [SerializeField]
+    private float m_TimeBeforeReset;
+    private float m_Timer;
+    private bool m_IsPressedDown = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_IsPressedDown = true;
+            m_Timer = 0.0f;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            m_IsPressedDown = false;
+        }
+
+        if (m_IsPressedDown)
+        {
+            m_Timer += Time.deltaTime;
+
+            if (m_Timer > m_TimeBeforeReset)
+            {
+                LevelDirector.Instance.CompleteLevel(false);
+                m_IsPressedDown = false;
+                m_Timer = 0.0f;
+            }
+        }
+    }
+}
