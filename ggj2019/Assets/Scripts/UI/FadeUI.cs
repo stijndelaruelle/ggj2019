0dacd980b6741e65db8da7ebbbd0630fe369ae18
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2019-01-27 11:30:38 +0100

    Added fade in & out to the main menu.

---
+++ ggj2019/Assets/Scripts/UI/FadeUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+[RequireComponent(typeof(Image))]
+public class FadeUI : MonoBehaviour
+{
+    private void Start()
+    {
+        Image image = GetComponent<Image>();
+        image.DOFade(0.0f, 1.0f);
+    }
+}
