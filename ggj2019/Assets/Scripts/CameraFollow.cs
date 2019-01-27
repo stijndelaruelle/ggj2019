7e796a2991e688269b8e2616b51a0cfaafa4cbbd
Aurelia <aurelia.philips@hotmail.com>
2019-01-27 03:20:19 +0100

    Dafs environment is in

---
+++ ggj2019/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	[SerializeField]
+	private Transform m_Target;
+
+	private void Update()
+	{
+		Vector3 pos = m_Target.position;
+		pos.z = transform.position.z;
+
+		transform.position = pos; 
+	}
+}
