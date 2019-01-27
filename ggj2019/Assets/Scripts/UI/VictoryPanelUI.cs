1622de8e78cd32f6cfe0b38cbfdf597d6e661520
Aurelia <aurelia.philips@hotmail.com>
2019-01-27 01:05:21 +0100

    Loop!

---
+++ ggj2019/Assets/Scripts/UI/VictoryPanelUI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Sjabloon;
+
+public class VictoryPanelUI : MonoBehaviour
+{
+	[SerializeField]
+	private CanvasGroup m_CanvasGroup;
+
+	private void Start()
+	{
+		LevelDirector levelDirector = LevelDirector.Instance;
+
+		if (levelDirector == null)
+			return;
+
+		levelDirector.LevelSucces += OnLevelSucces; 
+	}
+
+	private void OnDestroy()
+	{
+		LevelDirector levelDirector = LevelDirector.Instance;
+
+		if (levelDirector == null)
+			return;
+
+		levelDirector.LevelSucces -= OnLevelSucces; 
+	}
+
+	private void OnLevelSucces()
+	{
+		m_CanvasGroup.Show(true); 
+	}
+}
